dd3de3203a1a0bbaccfa6953d0e6849186a4620d
Shegofa <shogofadeveloper12@gmail.com>
2024-10-02 10:00:41 +0400

    Refactor Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
-import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
 
   return (
     <html lang="en">
